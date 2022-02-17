4d72f4c41f173398a39594ce53c9f741e0ec3f46
smallStall <91565962+smallStall@users.noreply.github.com>
2022-02-17 12:07:11 +0900

    Update root.tsx
    Add <Scripts />

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,7 @@ export default function Index() {
       <div>
         <p>------------------------------------------</p>
         <Outlet />
+        <Scripts />
         <p>------------------------------------------</p>
       </div>
     </Document>
